7d857587066425d82eb5b9017c200bcc6782003a
xuanzhi <736603914@qq.com>
2021-01-13 00:51:07 +0800

    创建基本

---
+++ assistant-service/src/main/java/com/lxz/assistant/AssistantServiceApplication.java
@@ -0,0 +1,13 @@
+package com.lxz.assistant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssistantServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AssistantServiceApplication.class, args);
+    }
+
+}
---
+++ assistant-service/src/test/java/com/lxz/assistant/AssistantServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lxz.assistant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssistantServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
